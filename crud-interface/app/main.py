ba32005fb492efb2c6a32ffe705d69cf93856522
Chew Wei Jin <chew@Chews-MacBook-Pro.local>
2025-03-30 20:53:18 +0800

    update startup event

--- crud-interface/app/main.py
+++ crud-interface/app/main.py
@@ -49,7 +49,7 @@ def read_root():
 def health_check():
     return {"status":"ok"}
 
-@app("startup")
+@app.on_event("startup")
 async def startup_event():
     try:
         # Create admin user if it doesn't exist
